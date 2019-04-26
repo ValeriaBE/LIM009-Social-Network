78eb04515c6f7c456a671eb644a9c8055cf153f2
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-04-26 06:26:43 -0500

    kagregando estilos para mobile first y botones login Facebook y google falta perfeccionar el login

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-// Este es el punto de entrada de tu aplicacion
+// // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
 const registro = document.getElementById('registrate');
 const container = document.getElementById('container');
 registro.addEventListener('click', () =>{
@@ -29,40 +29,7 @@ var config = {
     messagingSenderId: "815925731144"
   };
   firebase.initializeApp(config);
-// const bigOne = document.getElementById('bigOne');
-// const dbRef = firebase.database().ref().child('text');
-// dbRef.on('value', snap => bigOne.innerText = snap.val());
-
 let firestore= firebase.firestore();
-// const docRef =firestore.doc('Registro/Cuentas');
-// const email = document.getElementById('email');
-// const password = document.getElementById('password');
-// const acceder = document.getElementById('acceder');
-// acceder.addEventListener('click', ()=>{
-//     const textToSave = email.value;
-//     const passtoSave = password.value;
-//     console.log('I am going to save '+ textToSave + ' to firestore');
-//     docRef.set({
-//         Contraseña: passtoSave,
-//         Correo: textToSave
-//     }).then( ()=>{
-//         console.log('Status saved!');
-//     }).catch((error)=>{
-//         console.log('Got an error!', error);
-//     })
-// });
-
-// const correo = document.getElementById('correo');
-// const getRealtimeUpdates = () =>{
-//     docRef.onSnapshot((doc)=>{
-//         if(doc && doc.exists){
-//             const myData = doc.data();
-//             correo.innerHTML=myData.Correos;
-//         }
-//     })
-// };
-// getRealtimeUpdates();
-
 const form = document.getElementById('add-profile');
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
@@ -73,3 +40,24 @@ form.addEventListener('submit', (e)=>{
     form.email.value='';
     form.password.value='';
 })
+
+const showContent = user => {
+    const content = document.getElementById('content')
+    if (user.emailVerified) {
+        const string = `
+    <p>Welcome</p>
+    <button id="buttonLogOut">Cerrar sesión</button>`
+        const div = document.createElement('div')
+        div.innerHTML = string
+        content.appendChild(div)
+    }
+
+const onSignIn=(googleUser) =>{
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); 
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); 
+      }
+const SignIn = document.getElementById('Signin');
+SignIn.addEventListener('click',onSignIn)};
