44be1caf4ccb780ba0363e5b0b6ca3eb257393f5
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-04-28 13:54:03 -0500

    cambiando paleta de colores

--- src/lib/Content.js
+++ src/lib/Content.js
@@ -16,7 +16,7 @@ const main = document.getElementById('main');
 export const screen1 = () => {
 const loginPage = `  
 <figure class="top-image">
-  <img src="img/renew.jpg" alt="">
+  <img src="img/p.jpg" alt="">
 </figure>
 <div id='login'>
   <section class="bienvenida">
@@ -26,8 +26,11 @@ const loginPage = `
     <input type="email" name="correo" id="email-login" class="inputs" placeholder="Email">
     <input type="password" name="contraseña" id="password-login" class="inputs" placeholder="Password">
     <button id="login-btn" class="login">Log in</button>
-    <button id="google">Iniciar con Google</button>
-    <button id="fb">Iniciar con fb</button>
+    <div class="login-options">
+      <p class='text-color other-login'>O bien ingresa con...</p>
+      <button id="fb" class="login-buttons"><img src='img/fb.svg'></img></button>
+      <button id="google" class="login-buttons"><img src ='img/google.svg'></img></button>
+    </div>
   </form>
   <div class="text">
     <p class="text-color other-login">¿No tienes una cuenta?<a href="#" id="registrate"> Registrate</a></p>
