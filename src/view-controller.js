79e156a983ff526b797071c85c1a2d563b35c8c7
Maga <magabriela.lops@gmail.com>
2019-05-10 17:06:48 -0500

    config storage and comentar linea cdg unsuscribe()

--- src/view-controller.js
+++ src/view-controller.js
@@ -1,111 +1,112 @@
 import {
-    registerUser,
-    // checkEmail,
-    activeUser,
-    exit,
-    loginUser,
-    db,
-    getUser
+	registerUser,
+	// checkEmail,
+	activeUser,
+	exit,
+	loginUser,
+	db,
+	getUser
 } from './controller/controller-firebase.js'
 
 
 export const createUser = (cred) => {
-    return db().collection('users').doc(cred.user.uid).set({
-        name: cred.user.displayName
-    })
+	return db().collection('users').doc(cred.user.uid).set({
+		name: cred.user.displayName
+	})
 }
 
 export const registerInOnSubmit = () => {
-    const nameToSave = document.querySelector('[id="name-signup"]').value;
-    const email = document.querySelector('[id="email-signup"]').value;
-    const password = document.querySelector('[id="password-signup"]').value;
-    registerUser(email, password)
-        .then((cred) => {
-            return db().collection('users').doc(cred.user.uid).set({
-                name: nameToSave
-            })
-        })
-        .then(() => exit())
-        .then(() => changeRoute("#/home"))
+	const nameToSave = document.querySelector('[id="name-signup"]').value;
+	const email = document.querySelector('[id="email-signup"]').value;
+	const password = document.querySelector('[id="password-signup"]').value;
+	registerUser(email, password)
+		.then((cred) => {
+			return db().collection('users').doc(cred.user.uid).set({
+				name: nameToSave
+			})
+		})
+		.then(() => exit())
+		.then(() => changeRoute("#/home"))
 }
 
 export const savePostdb = (user) => {
-    let textPost = document.querySelector('#text-post').value;
-    let modoPost = document.querySelector('#visualización').value;
-    console.log(user);
-    getName(user).then((name)=>{
-        db().collection('posts').add({
-            uid : getUser().uid,
-            name: name,
-            texto: textPost,
-            modo: modoPost
-        })
-        .then((docRef) => {
-            console.log("Document written with ID: ", docRef.id);
-            textPost = '';
-            modoPost = '';
-        })
-        .catch(function (error) {
-            console.error("Error adding document: ", error);
-        });
-    })
+	let textPost = document.querySelector('#text-post').value;
+	let modoPost = document.querySelector('#visualización').value;
+	console.log(user);
+	getName(user).then((name) => {
+		db().collection('posts').add({
+				uid: getUser().uid,
+				name: name,
+				texto: textPost,
+				modo: modoPost
+			})
+			.then((docRef) => {
+				console.log("Document written with ID: ", docRef.id);
+				textPost = '';
+				modoPost = '';
+			})
+			.catch(function (error) {
+				console.error("Error adding document: ", error);
+			});
+	})
 }
 
 export const viewPostdb = (callback) => {
-    db().collection("posts").onSnapshot((querySnapshot) => {
-        const data = [];
-        querySnapshot.forEach((doc) => {
-            data.push({
-                id: doc.id,
-            ...doc.data()});
-            console.log(data);
-        });
-        callback(data);
-    });
- }
+	db().collection("posts").onSnapshot((querySnapshot) => {
+		const data = [];
+		querySnapshot.forEach((doc) => {
+			data.push({
+				id: doc.id,
+				...doc.data()
+			});
+			console.log(data);
+		});
+		callback(data);
+	});
+}
 
 export const exitUser = () => {
-    return exit()
+	return exit()
 }
 
 export const loginInOnSubmit = () => {
-    const email = document.querySelector('[id="email-login"]').value;
-    const password = document.querySelector('[id="password-login"]').value;
-    loginUser(email, password)
-        .then(() => changeRoute("#/profile"))
-        .catch(() => {
-            alert('Usuario invalido');
-        });
+	const email = document.querySelector('[id="email-login"]').value;
+	const password = document.querySelector('[id="password-login"]').value;
+	loginUser(email, password)
+		.then(() => changeRoute("#/profile"))
+		.catch(() => {
+			alert('Usuario invalido');
+		});
 }
 
 export const showUser = () => {
-    activeUser(changeRoute)
+	activeUser(changeRoute)
 }
 
 export const changeRoute = (route) => {
-    location.hash = route;
+	location.hash = route;
 }
 
 export const getName = (user) => {
-    if (user) {
-        if (user.providerData[0].providerId != 'password') {
-            return {
-                then: (cb) => {
-                    cb(user.displayName)
-                }
-            }
-        } else {
-            return db().collection('users').doc(user.uid).get()
-                .then((doc) => {
-                    return doc.data().name;
-                })
+	if (user) {
+		if (user.providerData[0].providerId != 'password') {
+			return {
+				then: (cb) => {
+					cb(user.displayName)
+				}
+			}
+		} else {
+			return db().collection('users').doc(user.uid).get()
+				.then((doc) => {
+					return doc.data().name;
+				})
 
-        }
-    }
+		}
+	}
 }
 
 export const deleteUser = (user) => {
-    user.delete().then(() => {
-        alert('Usuario eliminad@')
-    })
+	user.delete().then(() => {
+		alert('Usuario eliminad@')
+	})
 }
