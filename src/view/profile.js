1e89c49cda81b7d8efbc487f916064b9e0fc0d98
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-05-15 15:23:10 -0500

    agregando estilos al perfil cuando la pantalla es grande y agreganda la imagen por default

--- src/view/profile.js
+++ src/view/profile.js
@@ -1,49 +1,52 @@
 import{
-    exitUser, deleteUser, getName, changeRoute
+  exitUser, deleteUser, getName, changeRoute
 } from '../view-controller.js'
 import { getUser } from '../controller/controller-firebase.js';
 import { postScreen } from './posts.js';
 
 export const showActUser = (user, posts) => {
-    console.log(user);
-    const divContainer = document.createElement('div');
-    divContainer.innerHTML='';
-    const userCheck = user;
-    let string = `
-    <header class="header-profile back-color center flex text-color">
-        <div class="dropdown border-none text-color back-color">
-            <img class="menu-button back-color" src="img/menu.png" alt="">
-            <div class="dropdown-content position">
-            <a class="dropdown-links text-color block" href="#/home" id="exit">Cerrar Sesión</a>
-            <a class="dropdown-links text-color block" href="#/home" id="delete">Borrar Perfil</a>
-            </div>
-        </div>
-        <div class="position">
-            <p class="header-items back-color text-color inline-block">- Wenergy -</p>
-        </div>
-    </header>
-    <section class="margin center flex">
-        <figure class="figure-profile inline-block">
-            <img class="img-user"src="${userCheck.photoURL}" alt="foto">
-        </figure>
-        <div class="name-user name-color inline-block">
-            <p id="userName">${userCheck.displayName || userCheck.name}</p>
-            <p>${userCheck.email}<p>
-        </div>
-    </section>
-      `;
-      divContainer.innerHTML = string;
-      divContainer.classList.add('container2');
-      divContainer.appendChild(postScreen(posts));
-    
-    const buttonLogOut = divContainer.querySelector('#exit');
-    buttonLogOut.addEventListener('click', () => {
-      exitUser().then(()=> changeRoute("#/home"));
-    });
-    const buttonLDelete = divContainer.querySelector('#delete');
-    buttonLDelete.addEventListener('click', () => {
-      deleteUser(getUser());
-    });
-    return divContainer;
-  }
+  console.log(user);
+  const divContainer = document.createElement('div');
+  divContainer.innerHTML='';
+  const userCheck = user;
+  let string = `
+  <header class="header-profile back-color center flex text-color">
+      <div class="dropdown border-none text-color back-color">
+          <img class="menu-button back-color" src="img/menu.png" alt="">
+          <div class="dropdown-content position">
+          <a class="dropdown-links text-color block" href="#/home" id="exit">Cerrar Sesión</a>
+          <a class="dropdown-links text-color block" href="#/home" id="delete">Borrar Perfil</a>
+          </div>
+      </div>
+      <div class="position">
+          <p class="header-items back-color text-color inline-block">- Wenergy -</p>
+      </div>
+  </header>
+  <section class="margin big-profile center container">
+  <div class="flex auto main-img post-border info-box">
+      <figure class="photo-box inline-block">
+          <img class="img-user"src="${userCheck.photoURL == null ? 'img/No-Photo-Available.jpg': userCheck.photoURL}" alt="foto">
+      </figure>
+      <div class="name-user name-color inline-block">
+          <p id="userName">${userCheck.displayName || userCheck.name}</p>
+          <p>${userCheck.email}<p>
+      </div>
+  </div>
+      <div id='box-of-posts' class="delete-img margin"></div>
+  </section>
+    `;
+    divContainer.innerHTML = string;
+    divContainer.classList.add('container2');
+    const boxOfPosts = divContainer.querySelector('#box-of-posts');
+    boxOfPosts.appendChild(postScreen(posts));
   
+  const buttonLogOut = divContainer.querySelector('#exit');
+  buttonLogOut.addEventListener('click', () => {
+    exitUser().then(()=> changeRoute("#/home"));
+  });
+  const buttonLDelete = divContainer.querySelector('#delete');
+  buttonLDelete.addEventListener('click', () => {
+    deleteUser(getUser());
+  });
+  return divContainer;
+}
